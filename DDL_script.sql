22f14af3c2c7adbadf582b47ec15ae5e9f78a557
Sahil <sahillakdawala1109@gmail.com>
2023-04-15 23:11:31 +0530

    over to gor

--- DDL_script.sql
+++ DDL_script.sql
@@ -1,28 +1,33 @@
+DROP SCHEMA DBMS_PROJECT CASCADE;
 CREATE SCHEMA DBMS_PROJECT;
 SET search_path TO DBMS_PROJECT;
 
 CREATE TABLE "User" (
     User_id INT NOT NULL,
-    User_name VARCHAR(20) NOT NULL
+    User_name VARCHAR(20) NOT NULL,
+	PRIMARY KEY(User_id)
 );
 
 CREATE TABLE Project (
     Project_id INT NOT NULL,
     Project_name VARCHAR(20) NOT NULL,
-    Manager_id INT NOT NULL
+    Manager_id INT NOT NULL,
+	PRIMARY KEY(Project_id)
 );
 
 CREATE TABLE Collaborator (
     Project_id INT NOT NULL,
     User_id INT NOT NULL,
-    Role VARCHAR(20) NOT NULL
+    Role VARCHAR(20) NOT NULL,
+    PRIMARY KEY (Project_id, User_id)
 );
 
 CREATE TABLE Local_Files (
     Local_id INT NOT NULL,
     Project_id INT NOT NULL,
     User_id INT,
-    Timeline_id INT SET DEFAULT 0 NOT NULL
+    Timeline_id INT DEFAULT 0 NOT NULL,
+    PRIMARY KEY (Local_id, Project_id)
 );
 
 CREATE TABLE "File" (
@@ -30,7 +35,8 @@ CREATE TABLE "File" (
     Local_id INT NOT NULL,
     Project_id INT NOT NULL,
     File_name VARCHAR(20) NOT NULL,
-    Length INT NOT NULL
+    Length INT NOT NULL,
+    PRIMARY KEY (Local_id, Project_id)
 );
 
 CREATE TABLE "Line" (
@@ -38,21 +44,24 @@ CREATE TABLE "Line" (
     File_id INT NOT NULL,
     Local_id INT NOT NULL,
     Project_id INT NOT NULL,
-    Content VARCHAR(1024) NOT NULL
+    Content VARCHAR(1024) NOT NULL,
+    PRIMARY KEY (Local_id, Project_id, File_id)
 );
 
 CREATE TABLE Timeline (
     Timeline_id INT NOT NULL,
     Project_id INT NOT NULL,
     Latest_Version VARCHAR(20) NOT NULL,
-    Latest_Files VARCHAR(20) NOT NULL
+    Latest_Files VARCHAR(20) NOT NULL,
+    PRIMARY KEY (Project_id, Timeline_id)
 );
 
 CREATE TABLE "Version" (
     Version_id INT NOT NULL,
     Timeline_id INT NOT NULL,
     Project_id INT NOT NULL,
-    Updater_id INT NOT NULL
+    Updater_id INT NOT NULL,
+    PRIMARY KEY (Version_id, Timeline_id, Project_id)
 );
 
 CREATE TABLE Change (
@@ -62,7 +71,8 @@ CREATE TABLE Change (
     Line_id INT NOT NULL,
     File_id INT NOT NULL,
     Previous_content VARCHAR(1024) NOT NULL,
-    New_content VARCHAR(1024) NOT NULL
+    New_content VARCHAR(1024) NOT NULL,
+    PRIMARY KEY (Version_id, Timeline_id, Project_id, Line_id, File_id)
 );
 
 ALTER TABLE Collaborator
@@ -70,12 +80,12 @@ ALTER TABLE Collaborator
     ON DELETE CASCADE ON UPDATE CASCADE,
     ADD FOREIGN KEY (User_id) REFERENCES "User"(User_id)
     ON DELETE SET NULL ON UPDATE CASCADE;
-
+    
 ALTER TABLE Local_Files
     ADD FOREIGN KEY (Project_id) REFERENCES Project(Project_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
     ADD FOREIGN KEY (User_id) REFERENCES "User"(User_id)
-    ON DELETE SET NULL ON UPDATE CASCADE;
+    ON DELETE SET NULL ON UPDATE CASCADE,
     ADD FOREIGN KEY (Timeline_id) REFERENCES Timeline(Timeline_id)
     ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -101,8 +111,9 @@ ALTER TABLE "Version"
     ADD FOREIGN KEY (Timeline_id) REFERENCES Timeline(Timeline_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
     ADD FOREIGN KEY (Project_id) REFERENCES Project(Project_id)
-    ON DELETE CASCADE ON UPDATE CASCADE;
-     
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD FOREIGN KEY (Updater_id) REFERENCES "User"(User_id);
+
 ALTER TABLE Change
     ADD FOREIGN KEY (Version_id) REFERENCES "Version"(Version_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
